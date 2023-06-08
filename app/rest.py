1bf17909f862597b8ac79c9310ef1cdf310a89b2
joakofv <jfernandezvizcarra@gmail.com>
2023-06-08 17:27:29 -0400

    new commit

--- app/rest.py
+++ app/rest.py
@@ -60,6 +60,39 @@ def register_admin():
     con.close()
     return jsonify({"message":"Admin created"})
 
+# Create Company
+@app.route('/create_company',methods=['POST'])
+def create_company():
+    con = connection()
+    c = con.cursor()
+    data = request.get_json()
+    company_name = data['company_name']
+    company_api_key = data['company_api_key']
+    c.execute("INSERT INTO Company (Company_Name,Company_API_Key) VALUES (?,?)",(company_name,company_api_key))
+    if c.execute("SELECT * FROM Company WHERE Company_Name = ?",(company_name,)).fetchone() is not None:
+        return jsonify({"message":"Company already exist"}),400
+    con.commit()
+    con.close()
+    return jsonify({"message":"Company created"})
+
+# Get Company
+@app.route('/get_company',methods=['GET'])
+def get_company():
+    con = connection()
+    c = con.cursor()
+    c.execute("SELECT * FROM Company")
+    rows = c.fetchall()
+    con.close()
+    Company = []
+    for i in rows:
+        get_Company = {}
+        get_Company["ID"] = i["ID"]
+        get_Company["Company_Name"] = i["Company_Name"]
+        get_Company["Company_API_Key"] = i["Company_API_Key"]
+        Company.append(get_Company)
+    return jsonify(Company)
+
+
 if __name__ == "__main__":
     create_tables()
     app.run(debug=True,host='0.0.0.0')
