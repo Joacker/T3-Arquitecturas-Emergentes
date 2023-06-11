e3f0abfac9004d23b01bff963b1dc0dfe459a8fc
joakofv <jfernandezvizcarra@gmail.com>
2023-06-11 00:37:04 -0400

    new commit

--- app/auth.py
+++ app/auth.py
@@ -12,10 +12,27 @@ from flask_jwt_extended import (
 
 app.secret_key = "un_secreto"
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-jwt = JWTManager(app)
+jwt1 = JWTManager(app)
+
+# make decorator
+def token_required_company(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token-company' in request.headers:
+            token = request.headers['x-access-token-company']
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_company = data['username']
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(current_company, *args, **kwargs)
+    return decorated
 
 #GET ADMINS
-@app.route('/get_admin',methods=['GET'])
+@app.route('/get_admin', methods=['GET'])
 def get_admin():
     con = connection()
     c = con.cursor()
@@ -59,9 +76,6 @@ def register_admin():
     access_token = create_access_token(identity={"username":username}, expires_delta=datetime.timedelta(minutes=15))
 
     return jsonify({"username": username, "access_token": access_token}), 200
-
-@app.route("/login1", methods=["POST"])
-def login1():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     if username != "test" or password != "test":
@@ -69,6 +83,46 @@ def login1():
 
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
+
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200    
+
+
+@app.route("/login_company", methods=["POST"])
+def login_company():
+    con = connection()
+    c = con.cursor()
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    if not username:
+        return jsonify({"message":"Username is required"}),400
+    
+    if not password:
+        return jsonify({"message":"Password is required"}),400
+    
+    # validate if exist the company
+    company = c.execute("SELECT * FROM Admin WHERE Username = ?",(username,)).fetchone()
+    if company is None:
+        return jsonify({"message":"Company not exist"}),400
+    
+    if not check_password_hash(company["Password"],password):
+        return jsonify({"message":"Password is incorrect"}),400
+    
+    access_token = jwt.encode({"username":username}, app.config["SECRET_KEY"])
+    #print(app.secret_key)
+    con.close()
+    return jsonify({"token":access_token,"message":"Login successfull"})
+
+@app.route("/protected2", methods=["GET"])
+@token_required_company
+def protected2(current_company):
+    return jsonify(logged_in_as=current_company), 200
 
 #LOGIN ADMIN
 @app.route('/login',methods=['POST'])
